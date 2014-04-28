e67e9dd909022c70099d263a2a9d93983967ecd4
夏-舟 <sycamore.pw@gmail.com>
2014-04-28 14:05:19 +0800

    排序 分页

--- core/src/main/java/com/ssbusy/core/carbarn/service/CarbarnServiceImpl.java
+++ core/src/main/java/com/ssbusy/core/carbarn/service/CarbarnServiceImpl.java
@@ -20,23 +20,32 @@ import com.ssbusy.core.carbarn.domain.Carbarn;
 public class CarbarnServiceImpl implements CarbarnService {
 
 	private static final double EARTH_RADIUS = 6378.137;
+	private static final String SORT_BY_PRICE="price";
 
 	@Resource(name = "carbarnDao")
 	protected CarbarnDao carbarnDao;
 
 	@Override
 	public List<Carbarn> readCarbarnByCarbarnName(String carbarnName) {
-		return carbarnDao.readCarbarnByCarbarnName(carbarnName);
+		if ("".equals(carbarnName)) {
+			return null;
+		} else {
+			return carbarnDao.readCarbarnByCarbarnName(carbarnName);
+		}
 	}
 
 	@Override
 	public List<Carbarn> readCarbarnByCarbarnAddress(String carbarnAddress) {
-		return carbarnDao.readCarbarnByCarbarnAddress(carbarnAddress);
+		if ("".equals(carbarnAddress)) {
+			return null;
+		} else {
+			return carbarnDao.readCarbarnByCarbarnAddress(carbarnAddress);
+		}
 	}
 
 	@Override
 	public List<Carbarn> readCarbarnByLatitudeAndLongitude(
-			final Double latitude, final Double longitude) {
+			final Double latitude, final Double longitude,String sortBy) {
 		// TODO 这里设置经纬度的范围。
 		Double radius = 0.01666667d;
 		if (latitude == null || longitude == null) {
@@ -44,31 +53,34 @@ public class CarbarnServiceImpl implements CarbarnService {
 		}
 		List<Carbarn> carbarns = carbarnDao.readCarbarnByLatitudeAndLongitude(
 				latitude, latitude + radius, longitude, longitude + radius);
-		Collections.sort(carbarns, new Comparator<Carbarn>() {
-			@Override
-			public int compare(Carbarn carbarn1, Carbarn carbarn2) {
-				if (carbarn1.getCartEntrances().isEmpty()
-						|| carbarn2.getCartEntrances().isEmpty()) {
-					return 0;
+		if(SORT_BY_PRICE.equals(sortBy)){
+			Collections.sort(carbarns, new Comparator<Carbarn>() {
+				@Override
+				public int compare(Carbarn carbarn1, Carbarn carbarn2) {
+					return carbarn1.getPrice().compareTo(carbarn2.getPrice());
 				}
-				System.out.println(carbarn1.getAddress()+getDistance(latitude, longitude, carbarn1
-						.getCartEntrances().get(0).getLatitude(), carbarn1
-						.getCartEntrances().get(0).getLongitude()));
-				System.out.println(carbarn2.getAddress()+getDistance(latitude, longitude, carbarn2
-						.getCartEntrances().get(0).getLatitude(), carbarn2
-						.getCartEntrances().get(0).getLongitude()));
-				if (getDistance(latitude, longitude, carbarn1
-						.getCartEntrances().get(0).getLatitude(), carbarn1
-						.getCartEntrances().get(0).getLongitude()) > getDistance(
-						latitude, longitude, carbarn2.getCartEntrances().get(0)
-								.getLatitude(), carbarn2.getCartEntrances()
-								.get(0).getLongitude())) {
-					return 1;
-				} else {
-					return -1;
+			});
+		}else{
+			Collections.sort(carbarns, new Comparator<Carbarn>() {
+				@Override
+				public int compare(Carbarn carbarn1, Carbarn carbarn2) {
+					if (carbarn1.getCartEntrances().isEmpty()
+							|| carbarn2.getCartEntrances().isEmpty()) {
+						return 0;
+					}
+					if (getDistance(latitude, longitude, carbarn1
+							.getCartEntrances().get(0).getLatitude(), carbarn1
+							.getCartEntrances().get(0).getLongitude()) > getDistance(
+							latitude, longitude, carbarn2.getCartEntrances().get(0)
+									.getLatitude(), carbarn2.getCartEntrances()
+									.get(0).getLongitude())) {
+						return 1;
+					} else {
+						return -1;
+					}
 				}
-			}
-		});
+			});
+		}
 		return carbarns;
 	}
 

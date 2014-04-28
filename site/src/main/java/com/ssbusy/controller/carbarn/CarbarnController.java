52b13825dee9a3215c800bdbc21ebc743f3ade26
夏-舟 <sycamore.pw@gmail.com>
2014-04-28 14:06:48 +0800

    控制器和配置文件更改

--- site/src/main/java/com/ssbusy/controller/carbarn/CarbarnController.java
+++ site/src/main/java/com/ssbusy/controller/carbarn/CarbarnController.java
@@ -1,6 +1,8 @@
 package com.ssbusy.controller.carbarn;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ssbusy.core.carbarn.domain.Carbarn;
 import com.ssbusy.core.carbarn.service.CarbarnService;
@@ -25,6 +28,8 @@ import com.ssbusy.core.carbarn.service.CarbarnService;
 @Controller
 public class CarbarnController {
 
+	private int pageSize = 10;
+
 	@Resource(name = "carbarnService")
 	protected CarbarnService carbarnService;
 	protected String ak = "F86bd27c9f27ad2db50f6802ef6809d1";
@@ -32,66 +37,112 @@ public class CarbarnController {
 	protected String localUrl = "http://api.map.baidu.com/geosearch/v3/local";
 	protected String nearby = "http://api.map.baidu.com/geosearch/v3/nearby";
 
-	@RequestMapping("/carbarn/carbarn-name")
-	public String readCarbarnsByCarbarnName(HttpServletRequest request,
-			HttpServletResponse response, Model model,@RequestParam("carbarn_name") String carbarnName) {
-		List<Carbarn> carbarns = carbarnService.readCarbarnByCarbarnName(carbarnName);
-		JsonConfig  jsonConfig = new JsonConfig();
-		jsonConfig.setExcludes(new String[]{"carbarn"});
-		JSONArray jsonArray = JSONArray.fromObject(carbarns,jsonConfig);
-		return jsonArray.toString();
-	}
-	@RequestMapping("/carbarn/carbarn-address")
-	public String readCarbarnsByCarbarnAddress(HttpServletRequest request,
-			HttpServletResponse response, Model model,@RequestParam("carbarn_address") String carbarnAddress) {
-		List<Carbarn> carbarns = carbarnService.readCarbarnByCarbarnAddress(carbarnAddress);
-		JsonConfig  jsonConfig = new JsonConfig();
-		jsonConfig.setExcludes(new String[]{"carbarn"});
-		JSONArray jsonArray = JSONArray.fromObject(carbarns,jsonConfig);
-		return jsonArray.toString();
-	}
-	
-	@RequestMapping("/carbarn/latitude-longitude")
-	public String readCarbarnsByLatitudeAndLongitude(HttpServletRequest request,
-			HttpServletResponse response, Model model,@RequestParam("latitude") Double latitude,@RequestParam("longitude") Double longitude) {
+	@RequestMapping(value = "/carbarn/carbarn-name", produces = { "application/json;charset=UTF-8" })
+	@ResponseBody
+	public String readCarbarnsByCarbarnName(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Model model,
+			@RequestParam("carbarn_name") String carbarnName,
+			@RequestParam(value = "page_show", required = false) Integer pageShow) {
 		List<Carbarn> carbarns = carbarnService
-				.readCarbarnByLatitudeAndLongitude(latitude, longitude);
-		JsonConfig  jsonConfig = new JsonConfig();
-		jsonConfig.setExcludes(new String[]{"carbarn"});
-		JSONArray jsonArray = JSONArray.fromObject(carbarns,jsonConfig);
+				.readCarbarnByCarbarnName(carbarnName);
+		List<Carbarn> returnCarbarns = null;
+		returnCarbarns = showPage(pageShow, carbarns);
+		JsonConfig jsonConfig = new JsonConfig();
+		jsonConfig.setExcludes(new String[] { "carbarn" });
+		JSONArray jsonArray = JSONArray.fromObject(returnCarbarns, jsonConfig);
+		Map<String, Object> pageCount = new HashMap<String, Object>();
+		pageCount.put("pageCount", getPageCount(carbarns.size()));
+		jsonArray.add(pageCount);
 		return jsonArray.toString();
 	}
-	
 
-/*	@RequestMapping("/carbarn/search/nearbyby")
-	public String searchByPosition(String name) {
-		Map<String, String> sPara = new HashMap<String, String>();
-		sPara.put("region", "黄埔");
-		sPara.put("ak", "F86bd27c9f27ad2db50f6802ef6809d1");
-		sPara.put("geotable_id", tableId);
-		SsbusyHttpRequest httpRequest = new SsbusyHttpRequest();
-		SsbusyHttpResponse response = null;
-		httpRequest.setContentEncoding("utf-8");
-		try {
-			response = httpRequest.sendGet(localUrl, sPara);
-		} catch (IOException e) {
-			e.printStackTrace();
+	private List<Carbarn> showPage(Integer pageShow, List<Carbarn> carbarns) {
+		List<Carbarn> returnCarbarns;
+		int pageCount = getPageCount(carbarns.size());
+		if (pageShow == null || pageShow > pageCount) {
+			pageShow = 1;
 		}
-		JSONObject jsonObject = JSONObject.fromObject(response.getContent());
-		JSONArray jsonArray = jsonObject.getJSONArray("contents");
-		List<CarbarnForm> carbarnForms = new ArrayList<CarbarnForm>();
-		for (int i = 0; i < jsonArray.size(); i++) {
-			JSONObject carbarnFormObject = (JSONObject) jsonArray.get(i);
-			CarbarnForm carbarnForm = (CarbarnForm) JSONObject.toBean(
-					carbarnFormObject, CarbarnForm.class);
-			if (carbarnForm != null) {
-				carbarnForms.add(carbarnForm);
-			}
+		if (pageShow > 0 && pageShow < pageCount) {
+			returnCarbarns = carbarns.subList((pageShow - 1) * pageSize,
+					pageShow * pageSize);
+		} else if (pageShow == pageCount) {
+			returnCarbarns = carbarns.subList((pageShow - 1) * pageSize,
+					carbarns.size());
+		} else {
+			returnCarbarns = carbarns;
 		}
-		CarbarnTotalForm carbarnTotalForm = (CarbarnTotalForm) JSONObject
-				.toBean(jsonObject, CarbarnTotalForm.class);
-		carbarnTotalForm.setContents(carbarnForms);
-		String res = JSONObject.fromObject(carbarnTotalForm).toString();
-		return res;
-	}*/
+		return returnCarbarns;
+	}
+
+	@RequestMapping(value = "/carbarn/carbarn-address", produces = { "application/json;charset=UTF-8" })
+	@ResponseBody
+	public String readCarbarnsByCarbarnAddress(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Model model,
+			@RequestParam("carbarn_address") String carbarnAddress,
+			@RequestParam(value = "page_show", required = false) Integer pageShow) {
+		List<Carbarn> carbarns = carbarnService
+				.readCarbarnByCarbarnAddress(carbarnAddress);
+		List<Carbarn> returnCarbarns = null;
+		returnCarbarns = showPage(pageShow, carbarns);
+		JsonConfig jsonConfig = new JsonConfig();
+		jsonConfig.setExcludes(new String[] { "carbarn" });
+		JSONArray jsonArray = JSONArray.fromObject(returnCarbarns, jsonConfig);
+		Map<String, Object> pageCount = new HashMap<String, Object>();
+		pageCount.put("pageCount", getPageCount(carbarns.size()));
+		jsonArray.add(pageCount);
+		return jsonArray.toString();
+	}
+
+	@RequestMapping(value = "/carbarn/latitude-longitude", produces = { "application/json;charset=UTF-8" })
+	@ResponseBody
+	public String readCarbarnsByLatitudeAndLongitude(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Model model,
+			@RequestParam("latitude") Double latitude,
+			@RequestParam("longitude") Double longitude,
+			@RequestParam(value = "page_show", required = false) Integer pageShow,
+			@RequestParam(value = "sortBy", required = false) String sortBy) {
+		List<Carbarn> carbarns = carbarnService
+				.readCarbarnByLatitudeAndLongitude(latitude, longitude,sortBy);
+		List<Carbarn> returnCarbarns = null;
+		returnCarbarns = showPage(pageShow, carbarns);
+		JsonConfig jsonConfig = new JsonConfig();
+		jsonConfig.setExcludes(new String[] { "carbarn" });
+		JSONArray jsonArray = JSONArray.fromObject(returnCarbarns, jsonConfig);
+		Map<String, Object> pageCount = new HashMap<String, Object>();
+		pageCount.put("pageCount", getPageCount(carbarns.size()));
+		jsonArray.add(pageCount);
+		return jsonArray.toString();
+	}
+
+	protected int getPageCount(int size) {
+		return (size / (pageSize + 1)) + 1;
+	}
+	/*
+	 * @RequestMapping("/carbarn/search/nearbyby") public String
+	 * searchByPosition(String name) { Map<String, String> sPara = new
+	 * HashMap<String, String>(); sPara.put("region", "黄埔"); sPara.put("ak",
+	 * "F86bd27c9f27ad2db50f6802ef6809d1"); sPara.put("geotable_id", tableId);
+	 * SsbusyHttpRequest httpRequest = new SsbusyHttpRequest();
+	 * SsbusyHttpResponse response = null;
+	 * httpRequest.setContentEncoding("utf-8"); try { response =
+	 * httpRequest.sendGet(localUrl, sPara); } catch (IOException e) {
+	 * e.printStackTrace(); } JSONObject jsonObject =
+	 * JSONObject.fromObject(response.getContent()); JSONArray jsonArray =
+	 * jsonObject.getJSONArray("contents"); List<CarbarnForm> carbarnForms = new
+	 * ArrayList<CarbarnForm>(); for (int i = 0; i < jsonArray.size(); i++) {
+	 * JSONObject carbarnFormObject = (JSONObject) jsonArray.get(i); CarbarnForm
+	 * carbarnForm = (CarbarnForm) JSONObject.toBean( carbarnFormObject,
+	 * CarbarnForm.class); if (carbarnForm != null) {
+	 * carbarnForms.add(carbarnForm); } } CarbarnTotalForm carbarnTotalForm =
+	 * (CarbarnTotalForm) JSONObject .toBean(jsonObject,
+	 * CarbarnTotalForm.class); carbarnTotalForm.setContents(carbarnForms);
+	 * String res = JSONObject.fromObject(carbarnTotalForm).toString(); return
+	 * res; }
+	 */
 }
